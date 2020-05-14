5b3afd48500ee34ccdef2c8654838c9b2b0eba74
emerykurt <emerylumsden@gmail.com>
2020-05-14 16:03:32 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Dog
+    def name=(dogs_name)
+        @puts_name_here = dogs_name
+    end
+
+    def name
+        @puts_name_here
+    end
+
+    def bark
+        puts "woof!"
+    end
+end
